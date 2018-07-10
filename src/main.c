516f64b0a0df96a3b63d62ad912869b7a6aeaea0
marcburtbu <marcburt@bu.edu>
2018-07-10 18:53:25 -0400

    initial commit

---
+++ src/main.c
@@ -0,0 +1,305 @@
+/***************************************************************************/ /**
+
+  @file         main.c
+
+  @author       Marc Burt
+
+  @date         Thursday,  10 July 2018
+
+  @brief        BurtShell
+
+*******************************************************************************/
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+  Function Declarations for builtin shell commands:
+ */
+int burt_cd(char **args);
+int burt_help(char **args);
+int burt_exit(char **args);
+
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"};
+
+int (*builtin_func[])(char **) = {
+    &burt_cd,
+    &burt_help,
+    &burt_exit};
+
+int burt_num_builtins()
+{
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations.
+*/
+
+/**
+   @brief Bultin command: change directory.
+   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @return Always returns 1, to continue executing.
+ */
+int burt_cd(char **args)
+{
+  if (args[1] == NULL)
+  {
+    fprintf(stderr, "burt: expected argument to \"cd\"\n");
+  }
+  else
+  {
+    if (chdir(args[1]) != 0)
+    {
+      perror("burt");
+    }
+  }
+  return 1;
+}
+
+/**
+   @brief Builtin command: print help.
+   @param args List of args.  Not examined.
+   @return Always returns 1, to continue executing.
+ */
+int burt_help(char **args)
+{
+  int i;
+  printf("Marc Burt's BurtShell\n");
+  printf("Type program names and arguments, and hit enter.\n");
+  printf("The following are built in:\n");
+
+  for (i = 0; i < burt_num_builtins(); i++)
+  {
+    printf("  %s\n", builtin_str[i]);
+  }
+
+  printf("Use the man command for information on other programs.\n");
+  return 1;
+}
+
+/**
+   @brief Builtin command: exit.
+   @param args List of args.  Not examined.
+   @return Always returns 0, to terminate execution.
+ */
+int burt_exit(char **args)
+{
+  return 0;
+}
+
+/**
+  @brief Launch a program and wait for it to terminate.
+  @param args Null terminated list of arguments (including program).
+  @return Always returns 1, to continue execution.
+ */
+int burt_launch(char **args)
+{
+  pid_t pid;
+  int status;
+
+  pid = fork();
+  if (pid == 0)
+  {
+    // Child process
+    if (execvp(args[0], args) == -1)
+    {
+      perror("burt");
+    }
+    exit(EXIT_FAILURE);
+  }
+  else if (pid < 0)
+  {
+    // Error forking
+    perror("burt");
+  }
+  else
+  {
+    // Parent process
+    do
+    {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+/**
+   @brief Execute shell built-in or launch program.
+   @param args Null terminated list of arguments.
+   @return 1 if the shell should continue running, 0 if it should terminate
+ */
+int burt_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL)
+  {
+    // An empty command was entered.
+    return 1;
+  }
+
+  for (i = 0; i < burt_num_builtins(); i++)
+  {
+    if (strcmp(args[0], builtin_str[i]) == 0)
+    {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return burt_launch(args);
+}
+
+#define BURT_RL_BUFSIZE 1024
+/**
+   @brief Read a line of input from stdin.
+   @return The line from stdin.
+ */
+char *burt_read_line(void)
+{
+  int bufsize = BURT_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer)
+  {
+    fprintf(stderr, "burt: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1)
+  {
+    // Read a character
+    c = getchar();
+
+    if (c == EOF)
+    {
+      exit(EXIT_SUCCESS);
+    }
+    else if (c == '\n')
+    {
+      buffer[position] = '\0';
+      return buffer;
+    }
+    else
+    {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize)
+    {
+      bufsize += BURT_RL_BUFSIZE;
+      buffer = realloc(buffer, bufsize);
+      if (!buffer)
+      {
+        fprintf(stderr, "burt: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+}
+
+#define BURT_TOK_BUFSIZE 64
+#define BURT_TOK_DELIM " \t\r\n\a"
+/**
+   @brief Split a line into tokens (very naively).
+   @param line The line.
+   @return Null-terminated array of tokens.
+ */
+char **burt_split_line(char *line)
+{
+  int bufsize = BURT_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char *));
+  char *token, **tokens_backup;
+
+  if (!tokens)
+  {
+    fprintf(stderr, "burt: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, BURT_TOK_DELIM);
+  while (token != NULL)
+  {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize)
+    {
+      bufsize += BURT_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char *));
+      if (!tokens)
+      {
+        free(tokens_backup);
+        fprintf(stderr, "burt: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, BURT_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+
+  int token_size = sizeof(tokens);
+  int i;
+  for (i = 0; i < token_size; i++)
+  {
+    printf("%i%s\n", i, tokens[i]);
+  }
+  // printf("%ld\n", sizeof(tokens));
+  return tokens;
+}
+
+/**
+   @brief Loop getting input and executing it.
+ */
+void burt_loop(void)
+{
+  char *line;
+  char **args;
+  int status;
+
+  do
+  {
+    printf("MB> ");
+    line = burt_read_line();
+    args = burt_split_line(line);
+    status = burt_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+/**
+   @brief Main entry point.
+   @param argc Argument count.
+   @param argv Argument vector.
+   @return status code
+ */
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+
+  // Run command loop.
+  burt_loop();
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
+}
